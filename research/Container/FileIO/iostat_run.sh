54f1d580e237118761629482267cf801ed1368e9
jujuwon <webmon@kakao.com>
2022-03-07 09:27:28 +0900

    fix: flush cache

--- research/Container/FileIO/iostat_run.sh
+++ research/Container/FileIO/iostat_run.sh
@@ -2,40 +2,47 @@
 # 1개에서 16개까지 컨테이너를 동시실행하기.
 # 컨테이너 실행 시 iostat 실행 >> log 파일로 떨구기
 
-echo `iostat 1 30 -p /dev/nvme0n1 >> iostat_log1.txt &`
+echo `sync && echo 3 > /proc/sys/vm/drop_caches`
 sleep 1s
+echo `iostat 1 30 -p /dev/nvme0n1 >> iostat_log1.txt &`
+sleep 2s
 echo `docker run --rm --name nos_test1 -d nos_dummy 1`
-
 sleep 40s
 
+echo `sync && echo 3 > /proc/sys/vm/drop_caches`
+sleep 1s
 echo `iostat 1 30 -p /dev/nvme0n1 >> iostat_log2.txt &`
+sleep 2s
 for i in 1 2
 do
     echo `docker run --rm --name nos_test$i -d nos_dummy $i`
 done
-
 sleep 40s
 
-echo `iostat 1 30 -p /dev/nvme0n1 >> iostat_log4.txt &`
+echo `sync && echo 3 > /proc/sys/vm/drop_caches`
 sleep 1s
+echo `iostat 1 30 -p /dev/nvme0n1 >> iostat_log4.txt &`
+sleep 2s
 for i in {1..4}
 do
     echo `docker run --rm --name nos_test$i -d nos_dummy $i`
 done
-
 sleep 40s
 
-echo `iostat 1 30 -p /dev/nvme0n1 >> iostat_log8.txt &`
+echo `sync && echo 3 > /proc/sys/vm/drop_caches`
 sleep 1s
+echo `iostat 1 30 -p /dev/nvme0n1 >> iostat_log8.txt &`
+sleep 2s
 for i in {1..8}
 do
     echo `docker run --rm --name nos_test$i -d nos_dummy $i`
 done
-
 sleep 40s
 
-echo `iostat 1 30 -p /dev/nvme0n1 >> iostat_log16.txt &`
+echo `sync && echo 3 > /proc/sys/vm/drop_caches`
 sleep 1s
+echo `iostat 1 30 -p /dev/nvme0n1 >> iostat_log16.txt &`
+sleep 2s
 for i in {1..16}
 do
     echo `docker run --rm --name nos_test$i -d nos_dummy $i`
